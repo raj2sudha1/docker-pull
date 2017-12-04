21817d0588af64f769e7a04c736bd731f2bcbb42
sudhakar <raj2sudha1@gmail.com>
2017-12-04 10:15:45 +0000

    added new files

---
+++ dockerpull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull raj2sudha/iothub-poc
+#docker stop iothub-poc
+#docker run --rm --privileged --name iothub-poc -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /usr/lib/arm-linux-gnueabihf/libltdl.so.7:/usr/lib/arm-linux-gnueabihf/libltdl.so.7 -v /usr/local/test1:/usr/local/test1 -it raj2sudha/iothub-poc:latest
+
+
+
+
+
---
+++ dockerstop.js
@@ -0,0 +1,20 @@
+                        var Client = require('azure-iothub').Client;
+                        var connectionString = 'HostName=IOTCOPSY.azure-devices.net;SharedAccessKeyN
+ame=iothubowner;SharedAccessKey=2voH08x10F8oruR5QdyW3cvdCXfTbSAg8zFcNVE+S8E=';
+                        var methodName = "WriteLine";
+                        var deviceId = 'IOTCOP_Raspberry';
+                        var client=Client.fromConnectionString(connectionString);
+                        var methodParams = {
+                                methodName:methodName,
+                                payload: 'docker pull',
+                                timeoutInSeconds:60
+                        };
+                        client.invokeDeviceMethod(deviceId, methodParams, function (err, result){
+                                if(err){
+                                 console.error("failed to invoke method \'" + methodName + "\': " +
+err.message);
+                                 }else{
+                                  console.log(methodName + ' on ' + deviceId + ' : ');
+                                  console.log(JSON.stringify(result, null, 2));
+                                 }
+                        });
